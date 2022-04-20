80355bf722983ac6569b4528f1909dae7bb9dfda
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2022-04-21 00:05:43 +0100

    Small adjustment

--- html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['menu_0',['Menu',['../class_menu.html',1,'Menu'],['../class_menu.html#ad466dd83355124a6ed958430450bfe94',1,'Menu::Menu()']]]
-];
--- html/search/related_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['operator_3c_3c_0',['operator&lt;&lt;',['../class_encomenda.html#a3da07764e5876e23cde4ccc1fa85cf61',1,'Encomenda::operator&lt;&lt;()'],['../class_estafeta.html#a015f396274df71031096a245b4e9442a',1,'Estafeta::operator&lt;&lt;()']]]
-];
